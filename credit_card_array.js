88d212a34d3bf20ee27171a0de1a7ca498d541ce
dubbaka9 <dubbaka009@gmail.com>
2025-05-08 09:34:29 -0700

    updated jsom

--- credit_card_array.js
+++ credit_card_array.js
@@ -1,6 +1,6 @@
 const creditCardOffers = [
   {
-    "offerId": "OFF-PLT-2025-05",
+    "offerId": "OFF-PLT-2025-01",
     "title": "U.S. BANK CASH+® VISA SIGNATURE® CARD",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-cash-plus-signature-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>Earn <span className='sr-only'>a bonus that has increased from</span> <strike>$150</strike> <span className='sr-only'>to</span> $200 cash back.<sup className='disc_footnote'><a href='#footnote_region_cd87' aria-label='Footnote 1' id='returnto_bodynotes_2d6a'>1</a></sup></h4></div>",
@@ -9,7 +9,7 @@ const creditCardOffers = [
     "prediction": "high"
   },
   {
-    "offerId": "OFF-PLT-2025-06",
+    "offerId": "OFF-PLT-2025-02",
     "title": "U.S. BANK ALTITUDE® GO VISA SIGNATURE® CARD",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-altitude-go-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>4X points on dining, takeout and food delivery.<sup className='disc_footnote'><a href='#footnote_region_a281' aria-label='Footnote 1' id='returnto_bodynotes_a281'>1</a></sup></h4></div>",
@@ -18,7 +18,7 @@ const creditCardOffers = [
     "prediction": "medium"
   },
   {
-    "offerId": "OFF-PLT-2025-07",
+    "offerId": "OFF-PLT-2025-03",
     "title": "U.S. BANK ALTITUDE® CONNECT VISA SIGNATURE® CARD",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-altitude-connect-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>5X points on prepaid hotels and car rentals booked in the Altitude Rewards Center.<sup className='disc_footnote'><a href='#footnote_region_b156' aria-label='Footnote 1' id='returnto_bodynotes_b156'>1</a></sup></h4></div>",
@@ -27,7 +27,7 @@ const creditCardOffers = [
     "prediction": "high"
   },
   {
-    "offerId": "OFF-PLT-2025-08",
+    "offerId": "OFF-PLT-2025-04",
     "title": "U.S. BANK ALTITUDE® RESERVE VISA INFINITE® CARD",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-altitude-reserve-visa-infinite-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>Earn 5X points on prepaid hotels and car rentals booked directly in the Altitude Rewards Center.<sup className='disc_footnote'><a href='#footnote_region_f329' aria-label='Footnote 1' id='returnto_bodynotes_f329'>1</a></sup></h4></div>",
@@ -36,7 +36,7 @@ const creditCardOffers = [
     "prediction": "low"
   },
   {
-    "offerId": "OFF-PLT-2025-09",
+    "offerId": "OFF-PLT-2025-05",
     "title": "U.S. BANK SECURE VISA® CARD",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-secured-visa-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>Build credit with responsible use.<sup className='disc_footnote'><a href='#footnote_region_e472' aria-label='Footnote 1' id='returnto_bodynotes_e472'>1</a></sup></h4></div>",
@@ -45,7 +45,7 @@ const creditCardOffers = [
     "prediction": "high"
   },
   {
-    "offerId": "OFF-PLT-2025-10",
+    "offerId": "OFF-PLT-2025-6",
     "title": "U.S. BANK PLATINUM CARD",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-platinum-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>0% intro APR for 20 billing cycles on purchases and balance transfers.<sup className='disc_footnote'><a href='#footnote_region_d638' aria-label='Footnote 1' id='returnto_bodynotes_d638'>1</a></sup></h4></div>",
@@ -54,7 +54,7 @@ const creditCardOffers = [
     "prediction": "medium"
   },
   {
-    "offerId": "OFF-PLT-2025-11",
+    "offerId": "OFF-PLT-2025-7",
     "title": "U.S. BANK BUSINESS CASH REWARDS WORLD ELITE™ MASTERCARD®",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-business-cash-rewards-world-elite-mastercard-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>3% cash back on eligible purchases at gas stations, office supply stores and cell phone service providers.<sup className='disc_footnote'><a href='#footnote_region_c845' aria-label='Footnote 1' id='returnto_bodynotes_c845'>1</a></sup></h4></div>",
@@ -63,13 +63,31 @@ const creditCardOffers = [
     "prediction": "high"
   },
   {
-    "offerId": "OFF-PLT-2025-12",
+    "offerId": "OFF-PLT-2025-8",
     "title": "U.S. BANK BUSINESS PLATINUM CARD",
     "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-business-platinum-lg.png",
     "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>0% intro APR for 18 billing cycles on purchases and balance transfers.<sup className='disc_footnote'><a href='#footnote_region_h923' aria-label='Footnote 1' id='returnto_bodynotes_h923'>1</a></sup></h4></div>",
     "annualFee": "$0",
     "creditLimit": "$1,000 to $20,000",
     "prediction": "medium"
+  },
+  {
+    "offerId": "OFF-PLT-2025-9",
+    "title": "U.S. BANK TRIPLE CASH REWARDS VISA® BUSINESS CARD",
+    "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-triple-cash-rewards-visa-business-lg.png",
+    "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>Earn 3% cash back on eligible purchases at gas and EV charging stations, office supply stores, cell phone service providers and restaurants.<sup className='disc_footnote'><a href='#footnote_region_g745' aria-label='Footnote 1' id='returnto_bodynotes_g745'>1</a></sup></h4></div>",
+    "annualFee": "$0",
+    "creditLimit": "$1,000 to $25,000",
+    "prediction": "high"
+  },
+  {
+    "offerId": "OFF-PLT-2025-10",
+    "title": "U.S. BANK ALTITUDE® GO SECURED VISA® CARD",
+    "image": "https://it02.usbank.com/content/dam/usbank/en/images/photos/credit-cards/photo-altitude-go-secured-lg.png",
+    "descriptionHeading1": "<div className='mb-20-lg mb-12-md mb-24-sm'> <h4 className='display small'>4X points on dining, takeout and food delivery.<sup className='disc_footnote'><a href='#footnote_region_j591' aria-label='Footnote 1' id='returnto_bodynotes_j591'>1</a></sup></h4></div>",
+    "annualFee": "$0",
+    "creditLimit": "$300 to $5,000 (based on security deposit)",
+    "prediction": "high"
   }
 ];
 
